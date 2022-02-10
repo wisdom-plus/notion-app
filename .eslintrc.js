d0480ca22fead57508fbd9c5c546386126769115
tomoron <tomoron326@outlook.jp>
2022-02-10 21:00:57 +0900

    fix:format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,61 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "next", "next/core-web-vitals", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'next',
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
     'import/no-anonymous-default-export': 'off',
-    'import/order': ['error', {
-      alphabetize: {
-        order: 'asc'
-      }
-    }],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
     '@next/next/no-img-element': 'off',
     '@typescript-eslint/ban-types': 'warn',
     quotes: ['error', 'single'],
     'space-in-parens': ['error', 'never'],
     'no-multi-spaces': 'error',
-    'no-multiple-empty-lines': ['error', {
-      max: 1
-    }],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+      },
+    ],
     'comma-dangle': 'off',
     '@typescript-eslint/comma-dangle': ['error', 'never'],
     indent: 'off',
     '@typescript-eslint/indent': ['error', 2],
     'semi-style': ['error', 'last'],
-    'semi-spacing': ['error', {
-      after: true,
-      before: false
-    }],
+    'semi-spacing': [
+      'error',
+      {
+        after: true,
+        before: false,
+      },
+    ],
     semi: 'off',
     '@typescript-eslint/semi': ['error', 'always'],
     'no-extra-semi': 'off',
@@ -47,10 +66,13 @@ module.exports = {
     'no-useless-catch': 'off',
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/explicit-member-accessibility': ['warn', {
-      accessibility: 'explicit'
-    }],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'warn',
+      {
+        accessibility: 'explicit',
+      },
+    ],
     'no-unexpected-multiline': 'error',
-    'no-unreachable': 'error'
-  }
+    'no-unreachable': 'error',
+  },
 };
